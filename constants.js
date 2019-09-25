31675dfbbc12ccb478aeb50f7ec1bb1a4667a029
Eduardo Vieira <edusvieirap@gmail.com>
2019-09-25 12:50:02 -0300

    Adicionando constants.js

---
+++ constants.js
@@ -0,0 +1,59 @@
+TipoTeleconsultoria = {
+    ASSINCRONA: "A",
+    SINCRONA: "S"
+}
+
+CanalAcesso = {
+    INTERNET: "1",
+    TELEFONE: "2"
+}
+    
+GrauSatisfacao = {
+    MUITO_INSATISFEITO: "1",
+    INSATISFEITO: "2",
+    INDIFERENTE: "3",
+    SATISFEITO: "4",
+    MUITO_SATISFEITO: "5",
+    NAO_INFORMADO: "9"
+}
+    
+ResolucaoDuvidaTeleconsultoria = {
+    ATENDEU_TOTALMENTE: "1",
+    ATENDEU_PARCIALMENTE: "2",
+    NAO_ATENDEU: "3",
+    NAO_INFORMADO: "9"
+}
+    
+IntencaoEncaminhamentoTeleconsultoria = {
+    NAO: "0",
+    SIM: "1",
+    NAO_INFORMADO: "9"
+}
+
+EvitouEncaminhamentoTeleconsultoria = {
+    NAO: "0",
+    SIM: "1",
+    NAO_INFORMADO: "9"
+}
+
+TipoAtividade = {
+    CURSO: "1",
+    WEBAULAS_PALESTRAS: "2",
+    WEBSEMINARIOS: "3",
+    FORUM: "4",
+    REUNIAO: "5"
+}
+
+TipoObjetoAprendizagem = {
+    TEXTO: "1",
+    MULTIMIDIA: "2",
+    IMAGENS: "3",
+    APLICATIVOS: "4",
+    JOGOS_EDUCACIONAIS: "5",
+    OUTROS: "6"
+}
+
+Sexo = {
+    FEMININO: "F",
+    MASCULINO: "M"
+}

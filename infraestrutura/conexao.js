0964fb193d5c86f97b9b3389fc52a2d8c1d8be04
Eduardo Vieira <edusvieirap@gmail.com>
2020-02-07 15:53:56 -0300

    Update conexao.js

--- infraestrutura/conexao.js
+++ infraestrutura/conexao.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql')
 
 const conexao = mysql.createConnection({
-  host: '10.16.0.223',
-  port: 3306,
-  user: 'root',
-  password: 'Nut1s2012',
-  database: 'sints',
+  host: '',
+  port: '',
+  user: '',
+  password: '',
+  database: '',
   multipleStatements: true
 })
 
